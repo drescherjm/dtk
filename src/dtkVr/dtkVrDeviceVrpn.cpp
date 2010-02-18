7decd9c3a149dd65077d72b2d331a312cdf708a2
Julien Wintz <jwintz@sophia.inria.fr>
2010-02-18 20:26:17 +0100

    dtk vr device vrpn back port completed

--- src/dtkVr/dtkVrDeviceVrpn.cpp
+++ src/dtkVr/dtkVrDeviceVrpn.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Thu Feb 18 13:44:22 2010 (+0100)
  * Version: $Id$
- * Last-Updated: Thu Feb 18 16:35:02 2010 (+0100)
+ * Last-Updated: Thu Feb 18 20:21:23 2010 (+0100)
  *           By: Julien Wintz
- *     Update #: 50
+ *     Update #: 80
  */
 
 /* Commentary: 
@@ -26,6 +26,15 @@
 #include <vrpn_Analog.h>
 #include <vrpn_Tracker.h>
 #include <vrpn_FileConnection.h>
+#include <quat.h>
+
+// /////////////////////////////////////////////////////////////////
+// vrpn callbacks (Definition at EOF.)
+// /////////////////////////////////////////////////////////////////
+
+void VRPN_CALLBACK handle_button(void *data, const vrpn_BUTTONCB callback);
+void VRPN_CALLBACK handle_analog(void *data, const vrpn_ANALOGCB callback);
+void VRPN_CALLBACK handle_tracker(void *data, const vrpn_TRACKERCB callback);
 
 // /////////////////////////////////////////////////////////////////
 // dtkVrDeviceVrpnPrivate
@@ -36,6 +45,10 @@ class dtkVrDeviceVrpnPrivate : public QThread
 public:
     void run(void);
     void stop(void);
+
+    void handle_button(const vrpn_BUTTONCB callback);
+    void handle_analog(const vrpn_ANALOGCB callback);
+    void handle_tracker(const vrpn_TRACKERCB callback);
 
 public:
     bool running;
@@ -48,6 +61,8 @@ public:
     QList<dtkVrDeviceVrpn::dtkVrDeviceVrpnOrientationHandler> orientation_handlers;
 
     QUrl url;
+
+    dtkVrDeviceVrpn *q;
 };
 
 void dtkVrDeviceVrpnPrivate::run(void)
@@ -81,12 +96,57 @@ void dtkVrDeviceVrpnPrivate::stop(void)
     this->running = false;
 }
 
+void dtkVrDeviceVrpnPrivate::handle_button(const vrpn_BUTTONCB callback)
+{
+    switch(callback.button) {
+    case 1: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton1); break;
+    case 2: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton2); break;
+    case 3: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton3); break;
+    case 4: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton4); break;
+    case 5: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton5); break;
+    case 6: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton6); break;
+    case 7: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton7); break;
+    case 8: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton8); break;
+    case 9: emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButton9); break;
+    default:
+        emit q->buttonClicked(dtkVrDeviceVrpn::dtkVrDeviceVrpnButtonUndefined);
+        break;
+    };
+}
+
+void dtkVrDeviceVrpnPrivate::handle_analog(const vrpn_ANALOGCB callback)
+{
+    q->runPositionHandlers(callback.channel[0], callback.channel[1], callback.channel[2]);
+
+    q_type quat; q_from_euler(quat,
+        Q_DEG_TO_RAD(callback.channel[3]),
+        Q_DEG_TO_RAD(callback.channel[4]),
+        Q_DEG_TO_RAD(callback.channel[5]));
+
+    q->runOrientationHandlers(quat[0], quat[1], quat[2], quat[3]);
+}
+
+void dtkVrDeviceVrpnPrivate::handle_tracker(const vrpn_TRACKERCB callback)
+{
+    q->runPositionHandlers(
+        callback.pos[0],
+        callback.pos[1],
+        callback.pos[2]);
+
+    q->runOrientationHandlers(
+        callback.quat[0],
+        callback.quat[1],
+        callback.quat[2],
+        callback.quat[3]);
+}
+
 // /////////////////////////////////////////////////////////////////
 // dtkVrDeviceVrpn
 // /////////////////////////////////////////////////////////////////
 
 dtkVrDeviceVrpn::dtkVrDeviceVrpn(void) : dtkVrDevice(), d(new dtkVrDeviceVrpnPrivate)
 {
+    d->q = this;
     d->running = false;
 }
 
@@ -112,7 +172,7 @@ void dtkVrDeviceVrpn::registerOrientationHandler(dtkVrDeviceVrpn::dtkVrDeviceVrp
 
 QString dtkVrDeviceVrpn::description(void) const
 {
-    return QString();
+    return "dtkVrDeviceVrpn";
 }
 
 void dtkVrDeviceVrpn::startConnection(const QUrl& server)
@@ -129,10 +189,34 @@ void dtkVrDeviceVrpn::stopConnection(void)
 
 void dtkVrDeviceVrpn::runPositionHandlers(float x, float y, float z)
 {
-    
+    foreach(dtkVrDeviceVrpn::dtkVrDeviceVrpnPositionHandler handler, d->position_handlers)
+        (handler)(x, y, z);
 }
 
 void dtkVrDeviceVrpn::runOrientationHandlers(float q0, float q1, float q2, float q3)
 {
+    foreach(dtkVrDeviceVrpn::dtkVrDeviceVrpnOrientationHandler handler, d->orientation_handlers)
+        (handler)(q0, q1, q2, q3);
+}
 
+// /////////////////////////////////////////////////////////////////
+// vrpn callbacks
+// /////////////////////////////////////////////////////////////////
+
+void VRPN_CALLBACK handle_button(void *data, const vrpn_BUTTONCB callback)
+{
+    if(dtkVrDeviceVrpnPrivate *device = static_cast<dtkVrDeviceVrpnPrivate *>(data))
+        device->handle_button(callback);
+}
+
+void VRPN_CALLBACK handle_analog(void *data, const vrpn_ANALOGCB callback)
+{
+    if(dtkVrDeviceVrpnPrivate *device = static_cast<dtkVrDeviceVrpnPrivate *>(data))
+        device->handle_analog(callback);
+}
+
+void VRPN_CALLBACK handle_tracker(void *data, const vrpn_TRACKERCB callback)
+{
+    if(dtkVrDeviceVrpnPrivate *device = static_cast<dtkVrDeviceVrpnPrivate *>(data))
+        device->handle_tracker(callback);
 }
--- src/dtkVr/dtkVrDeviceVrpn.h
+++ src/dtkVr/dtkVrDeviceVrpn.h
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Thu Feb 18 13:43:20 2010 (+0100)
  * Version: $Id$
- * Last-Updated: Thu Feb 18 16:35:19 2010 (+0100)
+ * Last-Updated: Thu Feb 18 20:21:49 2010 (+0100)
  *           By: Julien Wintz
- *     Update #: 15
+ *     Update #: 19
  */
 
 /* Commentary: 
@@ -38,7 +38,23 @@ public:
     void registerPositionHandler(dtkVrDeviceVrpn::dtkVrDeviceVrpnPositionHandler handler);
     void registerOrientationHandler(dtkVrDeviceVrpn::dtkVrDeviceVrpnOrientationHandler handler);
 
+    enum Button {
+        dtkVrDeviceVrpnButton1,
+        dtkVrDeviceVrpnButton2,
+        dtkVrDeviceVrpnButton3,
+        dtkVrDeviceVrpnButton4,
+        dtkVrDeviceVrpnButton5,
+        dtkVrDeviceVrpnButton6,
+        dtkVrDeviceVrpnButton7,
+        dtkVrDeviceVrpnButton8,
+        dtkVrDeviceVrpnButton9,
+        dtkVrDeviceVrpnButtonUndefined
+    };
+
     QString description(void) const;
+
+signals:
+    void buttonClicked(Button button);
 
 public slots:
     void startConnection(const QUrl& server);
@@ -49,7 +65,7 @@ protected:
     void runOrientationHandlers(float q0, float q1, float q2, float q3);
 
 private:
-    friend class dtkVrDeviceVrpnPrivte; dtkVrDeviceVrpnPrivate *d;
+    friend class dtkVrDeviceVrpnPrivate; dtkVrDeviceVrpnPrivate *d;
 };
 
 #endif

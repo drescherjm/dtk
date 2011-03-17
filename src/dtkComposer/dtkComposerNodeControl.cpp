7aa650aa8768d6f2452eeaa5aaa55d3960d40751
Thibaud Kloczko <tkloczko@sophia.inria.fr>
2011-03-17 13:36:29 +0100

    Enabling node adjustment when decreasing control node size.

--- src/dtkComposer/dtkComposerNodeControl.cpp
+++ src/dtkComposer/dtkComposerNodeControl.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Mon Feb 28 12:49:38 2011 (+0100)
  * Version: $Id$
- * Last-Updated: Thu Mar 17 13:01:52 2011 (+0100)
+ * Last-Updated: Thu Mar 17 13:26:47 2011 (+0100)
  *           By: Thibaud Kloczko
- *     Update #: 414
+ *     Update #: 418
  */
 
 /* Commentary: 
@@ -244,6 +244,10 @@ void dtkComposerNodeControl::resize(const QPointF& delta)
 
 void dtkComposerNodeControl::resize(qreal dw, qreal dh)
 {
+    //static QPoint cursor_pos;
+
+    //cursor_pos = QCursor::pos();
+
     qreal dw_min =  this->minimalBoundingRect().width() -  this->boundingRect().width();
     qreal dh_min = this->minimalBoundingRect().height() - this->boundingRect().height();
 
@@ -254,6 +258,9 @@ void dtkComposerNodeControl::resize(qreal dw, qreal dh)
 
     foreach(dtkComposerNodeControlBlock *block, d->blocks)
         block->adjustChildNodes(dw, dh / d->blocks.count());
+
+    //if(true)
+    //    QCursor::setPos(cursor_pos - QPoint(dw, dh));
 }
 
 void dtkComposerNodeControl::resize(void)
--- src/dtkComposer/dtkComposerNodeControlBlock.cpp
+++ src/dtkComposer/dtkComposerNodeControlBlock.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Thu Mar  3 14:48:10 2011 (+0100)
  * Version: $Id$
- * Last-Updated: Thu Mar 17 13:04:56 2011 (+0100)
+ * Last-Updated: Thu Mar 17 13:35:04 2011 (+0100)
  *           By: Thibaud Kloczko
- *     Update #: 532
+ *     Update #: 542
  */
 
 /* Commentary: 
@@ -440,26 +440,19 @@ dtkComposerNodeProperty *dtkComposerNodeControlBlock::addOutputProperty(QString 
 
 void dtkComposerNodeControlBlock::adjustChildNodes(qreal dw, qreal dh)
 {
-    if (dw > 0 && dh > 0) {
-        qreal scaling_factor = 0.5;
+    qreal scaling_factor = 0.5;
+    qreal dx;
+    qreal dy;
+    qreal width;
+    qreal height;
 
-        qreal dx;
-        qreal dy;
-        qreal width;
-        qreal height;
-        foreach(dtkComposerNode *child, this->nodes()) {
-            width  = qAbs(child->mapRectToParent(child->boundingRect()).left() - this->boundingRect().left());
-            height = qAbs( child->mapRectToParent(child->boundingRect()).top() -  this->boundingRect().top());
-
-            qDebug() << child->mapRectToParent(child->boundingRect()).left() << child->mapRectToParent(child->boundingRect()).top() << this->boundingRect().left() << this->boundingRect().top();
-            qDebug() << child->pos().x() << child->pos().y() << this->pos().x() << this->pos().y();
-            qDebug() << width << height;
-
-            dx = scaling_factor * dw *  width /  this->boundingRect().width();
-            dy = scaling_factor * dh * height / this->boundingRect().height();
-
-            child->setPos(child->pos().x() + dx, child->pos().y() + dy);
-        }
+    foreach(dtkComposerNode *child, this->nodes()) {
+        width  = qAbs(child->mapRectToParent(child->boundingRect()).left() - this->boundingRect().left());
+        height = qAbs( child->mapRectToParent(child->boundingRect()).top() -  this->boundingRect().top());        
+        dx = scaling_factor * dw *  width /  this->boundingRect().width();
+        dy = scaling_factor * dh * height / this->boundingRect().height();
+        
+        child->setPos(child->pos().x() + dx, child->pos().y() + dy);
     }
 }
 
@@ -471,8 +464,8 @@ QRectF dtkComposerNodeControlBlock::minimalBoundingRect(void)
     if (this->nodes().count()) {
 
         QRectF child_rect = this->nodes().first()->mapRectToParent(this->nodes().first()->boundingRect());
-        qreal top  = child_rect.top();
-        qreal left = child_rect.left();
+        //qreal top  = child_rect.top();
+        //qreal left = child_rect.left();
         qreal bottom = child_rect.bottom();
         qreal right  = child_rect.right();
 
@@ -480,14 +473,17 @@ QRectF dtkComposerNodeControlBlock::minimalBoundingRect(void)
 
             child_rect = child->mapRectToParent(child->boundingRect());
 
-            top = top < child_rect.top() ? top : child_rect.top();
-            left = left < child_rect.left() ? left : child_rect.left();
+            //top = top < child_rect.top() ? top : child_rect.top();
+            //left = left < child_rect.left() ? left : child_rect.left();
             bottom = bottom > child_rect.bottom() ? bottom : child_rect.bottom();
             right = right > child_rect.right() ? right : child_rect.right();
         }
 
-        min_height = (1.1 * qAbs(top - bottom)) > min_height ? (1.1 * qAbs(top - bottom)) : min_height;
-        min_width  = (1.1 * qAbs(right - left)) >  min_width ? (1.1 * qAbs(right - left)) :  min_width;
+        //min_height = (1.1 * qAbs(top - bottom)) > min_height ? (1.1 * qAbs(top - bottom)) : min_height;
+        //min_width  = (1.1 * qAbs(right - left)) >  min_width ? (1.1 * qAbs(right - left)) :  min_width;
+
+        min_height = (1.1 * qAbs(this->rect().top() - bottom)) > min_height ? (1.1 * qAbs(this->rect().top() - bottom)) : min_height;
+        min_width  = (1.1 * qAbs(right - this->rect().left())) >  min_width ? (1.1 * qAbs(right - this->rect().left())) :  min_width;
     }
     return QRectF(this->rect().top(), this->rect().left(), min_width, min_height);
 }

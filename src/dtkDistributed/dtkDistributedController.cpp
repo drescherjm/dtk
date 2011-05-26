1ffd691d1970cd94870c70e8c77d9635716cbb89
Julien Wintz <julien.wintz@inria.fr>
2011-05-26 11:12:28 +0200

    Adding cross platform service framework based on qtservice solution.

--- src/dtkDistributed/dtkDistributedController.cpp
+++ src/dtkDistributed/dtkDistributedController.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Wed May 25 14:15:13 2011 (+0200)
  * Version: $Id$
- * Last-Updated: Wed May 25 17:14:54 2011 (+0200)
+ * Last-Updated: Thu May 26 09:19:18 2011 (+0200)
  *           By: Julien Wintz
- *     Update #: 28
+ *     Update #: 29
  */
 
 /* Commentary: 

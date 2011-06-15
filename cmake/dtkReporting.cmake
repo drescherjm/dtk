331b1af92df46d4e358f01afd10cdc8e7767bbb7
Benoît Bleuzé <benoit.bleuze@inria.fr>
2011-06-15 11:33:26 +0200

    Only configure project_nameDart.cmake and set associated variable if template file present in project or child project.

--- cmake/dtkReporting.cmake
+++ cmake/dtkReporting.cmake
@@ -16,28 +16,28 @@
 ### Change log:
 ## 
 ######################################################################
+if (EXISTS ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Dart.cmake.in)
+#only create test variables if the project contains a proj_nameDart.cmake.in template file.
+  set(DTK_CTEST_PROJECT_NAME ${PROJECT_NAME} CACHE STRING "Reporting project name.")
+  set(DTK_CTEST_UPDATE_TYPE "git" CACHE STRING "Reporting update type.")
+  set(DTK_CTEST_UPDATE_COMMAND "git" CACHE STRING "Reporting update command.")
+  set(DTK_CTEST_DROP_METHOD "http" CACHE STRING "Reporting drop method.")
+  set(DTK_CTEST_DROP_SITE "cdash.inria.fr" CACHE STRING "Reporting drop site.")
+  set(DTK_CTEST_DROP_LOCATION "/CDash/submit.php?project=${PROJECT_NAME}" CACHE STRING "Reporting drop location.")
+  set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_BINARY_DIR}/cmake")
 
-message(project name: ${PROJECT_NAME})
+  configure_file (
+    "${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Dart.cmake.in"
+    "${CMAKE_BINARY_DIR}/cmake/${PROJECT_NAME}Dart.cmake")
 
-set(DTK_CTEST_PROJECT_NAME ${PROJECT_NAME} CACHE STRING "Reporting project name.")
-set(DTK_CTEST_UPDATE_TYPE "git" CACHE STRING "Reporting update type.")
-set(DTK_CTEST_UPDATE_COMMAND "git" CACHE STRING "Reporting update command.")
-set(DTK_CTEST_DROP_METHOD "http" CACHE STRING "Reporting drop method.")
-set(DTK_CTEST_DROP_SITE "cdash.inria.fr" CACHE STRING "Reporting drop site.")
-set(DTK_CTEST_DROP_LOCATION "/CDash/submit.php?project=${PROJECT_NAME}" CACHE STRING "Reporting drop location.")
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_BINARY_DIR}/cmake")
+  mark_as_advanced(BUILD_TESTING)
+  mark_as_advanced(DART_TESTING_TIMEOUT)
+  mark_as_advanced(DTK_CTEST_PROJECT_NAME)
+  mark_as_advanced(DTK_CTEST_UPDATE_TYPE)
+  mark_as_advanced(DTK_CTEST_UPDATE_COMMAND)
+  mark_as_advanced(DTK_CTEST_DROP_METHOD)
+  mark_as_advanced(DTK_CTEST_DROP_SITE)
+  mark_as_advanced(DTK_CTEST_DROP_LOCATION)
 
-configure_file (
-  "${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Dart.cmake.in"
-  "${CMAKE_BINARY_DIR}/cmake/${PROJECT_NAME}Dart.cmake")
-
-mark_as_advanced(BUILD_TESTING)
-mark_as_advanced(DART_TESTING_TIMEOUT)
-mark_as_advanced(DTK_CTEST_PROJECT_NAME)
-mark_as_advanced(DTK_CTEST_UPDATE_TYPE)
-mark_as_advanced(DTK_CTEST_UPDATE_COMMAND)
-mark_as_advanced(DTK_CTEST_DROP_METHOD)
-mark_as_advanced(DTK_CTEST_DROP_SITE)
-mark_as_advanced(DTK_CTEST_DROP_LOCATION)
-
-include(${PROJECT_NAME}Dart)
+  include(${PROJECT_NAME}Dart)
+endif()

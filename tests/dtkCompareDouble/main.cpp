398b7b52f0765fb34d92b435cdbe7a5f809695e1
Thibaud Kloczko <tkloczko@sophia.inria.fr>
2011-06-30 13:31:07 +0200

    Modifying tests for comparing double and float in order to track likely errors.

--- tests/dtkCompareDouble/main.cpp
+++ tests/dtkCompareDouble/main.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2011 - Thibaud Kloczko, Inria.
  * Created: Wed Mar  9 14:08:06 2011 (+0100)
  * Version: $Id$
- * Last-Updated: Mon Jun 20 19:23:56 2011 (+0200)
- *           By: Julien Wintz
- *     Update #: 11
+ * Last-Updated: Thu Jun 30 13:30:12 2011 (+0200)
+ *           By: Thibaud Kloczko
+ *     Update #: 22
  */
 
 /* Commentary: 
@@ -31,44 +31,52 @@
 // 
 // /////////////////////////////////////////////////////////////////
 
-void TestCompare2sComplement(double A, double B, bool expectedResult, long long int maxUlps = 10)
+int TestCompare2sComplement(double A, double B, bool expectedResult, long long int maxUlps = 10)
 {
     bool result = dtkAlmostEqual2sComplement(A, B, maxUlps);
 
-    if (result != expectedResult)
+    if (result != expectedResult) {
         printf("Unexpected result - %1.18f, %1.18f, %lld, expected %s\n", A, B, maxUlps, expectedResult ? "true" : "false");
+        return DTK_FAILURE;
+    }
+
+    return DTK_SUCCEED;
 }
 
-void TestCompareFinal(double A, double B, bool expectedResult, long long int maxUlps = 10)
+int TestCompareFinal(double A, double B, bool expectedResult, long long int maxUlps = 10)
 {
     bool result = dtkAlmostEqualUlps(A, B, maxUlps);
 
-    if (result != expectedResult)
+    if (result != expectedResult) {
         printf("Unexpected result final - %1.18f, %1.18f, %lld, expected %s\n", A, B, maxUlps, expectedResult ? "true" : "false");
+        return DTK_FAILURE;
+    }
+
+    return DTK_SUCCEED;
 }
 
-void TestCompareAll(double A, double B, bool expectedResult, long long int maxUlps = 10)
+int TestCompareAll(double A, double B, bool expectedResult, long long int maxUlps = 10)
 {
-    TestCompare2sComplement(A, B, expectedResult, maxUlps);
-    TestCompareFinal(A, B, expectedResult, maxUlps);
+    if (TestCompare2sComplement(A, B, expectedResult, maxUlps) ||
+        TestCompareFinal(A, B, expectedResult, maxUlps))
+        return DTK_FAILURE;
+
+    return DTK_SUCCEED;
 }
 
 // /////////////////////////////////////////////////////////////////
 // 
 // /////////////////////////////////////////////////////////////////
 
-double zero1, zero2;
+double zero1 = 0;
+double zero2 = 0;
 
 int main(int argc, char* argv[])
 {
     double negativeZero;
-
-#if defined(DTK_PLATORM_64)
+        
     *(long long int*)&negativeZero = 0x8000000000000000LL;
-#else
-    *(long long int*)&negativeZero = 0x80000000LL;
-#endif
-
+    
     double nan1 = sqrt(-1.0);
     double nan2 = zero1 / zero2;
     double inf0 = 1 / zero1;
@@ -76,21 +84,26 @@ int main(int argc, char* argv[])
     double nan4 = nan3; (*(long long int*)&nan4) += 1;
     double smallestDenormal = 0; (*(long long int*)&smallestDenormal) += 1;
 
-    printf("01 - \n"); TestCompareAll(zero1, negativeZero, true);
-    printf("02 - \n"); TestCompareAll(2.0, 1.999999999999999, true);
-    printf("03 - \n"); TestCompareAll(2.0, 1.9999999999999995, true);
-    printf("04 - \n"); TestCompareAll(1.9999999999999995, 2.0, true);
-    printf("05 - \n"); TestCompareAll(2.0, 1.9999999999999995, true);
-    printf("06 - \n"); TestCompare2sComplement(inf0, -inf0, true, 16 * 1024 * 1024);
-    printf("07 - \n"); TestCompareFinal(inf0, -inf0, false, 16 * 1024 * 1024);
-    printf("08 - \n"); TestCompare2sComplement(DBL_MAX, inf0, true);
-    printf("09 - \n"); TestCompareFinal(DBL_MAX, inf0, false);
-    printf("10 - \n"); TestCompare2sComplement(nan2, nan2, true);
-    printf("11 - \n"); TestCompareFinal(nan2, nan2, false);
-    printf("12 - \n"); TestCompare2sComplement(nan2, nan3, true);
-    printf("13 - \n"); TestCompareFinal(nan2, nan3, false);
-    printf("14 - \n"); TestCompare2sComplement(smallestDenormal, -smallestDenormal, true);
-    printf("15 - \n"); TestCompareFinal(smallestDenormal, -smallestDenormal, false);
+    int status = 0;
+
+    printf("01 - \n"); status  = TestCompareAll(zero1, negativeZero, true);
+    printf("02 - \n"); status += TestCompareAll(2.0, 1.999999999999999, true);
+    printf("03 - \n"); status += TestCompareAll(2.0, 1.9999999999999995, true);
+    printf("04 - \n"); status += TestCompareAll(1.9999999999999995, 2.0, true);
+    printf("05 - \n"); status += TestCompareAll(2.0, 1.9999999999999995, true);
+    printf("06 - \n"); status += TestCompare2sComplement(inf0, -inf0, false, 16 * 1024 * 1024);
+    printf("07 - \n"); status += TestCompareFinal(inf0, -inf0, false, 16 * 1024 * 1024);
+    printf("08 - \n"); status += TestCompare2sComplement(DBL_MAX, inf0, true);
+    printf("09 - \n"); status += TestCompareFinal(DBL_MAX, inf0, false);
+    printf("10 - \n"); status += TestCompare2sComplement(nan2, nan2, true);
+    printf("11 - \n"); status += TestCompareFinal(nan2, nan2, false);
+    printf("12 - \n"); status += TestCompare2sComplement(nan2, nan3, true);
+    printf("13 - \n"); status += TestCompareFinal(nan2, nan3, false);
+    printf("14 - \n"); status += TestCompare2sComplement(smallestDenormal, -smallestDenormal, true);
+    printf("15 - \n"); status += TestCompareFinal(smallestDenormal, -smallestDenormal, false);
+
+    if (status)
+        return DTK_FAILURE;
 
     return DTK_SUCCEED;
 }

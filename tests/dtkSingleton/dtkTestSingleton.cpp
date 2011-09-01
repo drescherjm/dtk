dc1ca062ab0cd5d02fdeb0ee6050574187174d4b
Julien Wintz <jwintz@sophia.inria.fr>
2011-09-01 14:43:29 +0200

    Merging request #74.

--- tests/dtkSingleton/dtkTestSingleton.cpp
+++ tests/dtkSingleton/dtkTestSingleton.cpp
@@ -1,3 +1,21 @@
+/* dtkTestSingleton.cpp --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008-2011 - Julien Wintz, Inria.
+ * Created: Thu Aug 18 19:42:04 2011 (+0200)
+ * Version: $Id$
+ * Last-Updated: Thu Aug 18 19:42:06 2011 (+0200)
+ *           By: Julien Wintz
+ *     Update #: 1
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
 
 #include "dtkTestSingleton.h"
 

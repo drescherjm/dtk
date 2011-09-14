189ddeaeeda41d2b03ca315d0d14900e1e0a02fd
Julien Wintz <julien.wintz@inria.fr>
2011-09-14 13:21:13 +0200

    Adding example distributed tutorial step #4.

---
+++ tutorials/dtkDistributed/4/server_main.cpp
@@ -0,0 +1,25 @@
+/* server_main.cpp --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008-2011 - Julien Wintz, Inria.
+ * Created: Wed Sep 14 13:20:15 2011 (+0200)
+ * Version: $Id$
+ * Last-Updated: Wed Sep 14 13:20:24 2011 (+0200)
+ *           By: Julien Wintz
+ *     Update #: 2
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
+
+#include <QtCore>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
---
+++ tutorials/dtkDistributed/4/slave_main.cpp
@@ -0,0 +1,25 @@
+/* slave_main.cpp --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008-2011 - Julien Wintz, Inria.
+ * Created: Wed Sep 14 13:19:42 2011 (+0200)
+ * Version: $Id$
+ * Last-Updated: Wed Sep 14 13:20:05 2011 (+0200)
+ *           By: Julien Wintz
+ *     Update #: 3
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
+
+#include <QtCore>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}

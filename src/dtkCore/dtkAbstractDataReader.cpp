42735aa40099fdf6750cb08475b7426db1449ed8
Julien Wintz <julien.wintz@inria.fr>
2011-09-21 12:54:42 +0200

    Add access to initial file on which the reading process has been invoked, to be used in various reader specializations.

--- src/dtkCore/dtkAbstractDataReader.cpp
+++ src/dtkCore/dtkAbstractDataReader.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008 - Julien Wintz, Inria.
  * Created: Tue Feb 24 22:03:03 2009 (+0100)
  * Version: $Id$
- * Last-Updated: Thu Sep 15 15:11:44 2011 (+0200)
+ * Last-Updated: Wed Sep 21 12:52:26 2011 (+0200)
  *           By: Julien Wintz
- *     Update #: 38
+ *     Update #: 44
  */
 
 /* Commentary: 
@@ -28,6 +28,9 @@ public:
     bool enabled;
 
     dtkSmartPointer<dtkAbstractData> data;
+
+    QString file;
+    QStringList files;
 };
 
 dtkAbstractDataReader::dtkAbstractDataReader(void) : dtkAbstractObject(), d(new dtkAbstractDataReaderPrivate)
@@ -90,14 +93,14 @@ bool dtkAbstractDataReader::canRead(const QStringList& files)
 
 bool dtkAbstractDataReader::read(const QString& file)
 {
-    DTK_UNUSED(file);
+    d->file = file;
 
     return false;
 }
 
 bool dtkAbstractDataReader::read(const QStringList& files)
 {
-    DTK_UNUSED(files);
+    d->files = files;
 
     return false;
 }
@@ -116,4 +119,14 @@ void dtkAbstractDataReader::readInformation(const QStringList& paths)
 void dtkAbstractDataReader::setProgress(int value)
 {
     emit progressed (value);
+}
+
+const QString& dtkAbstractDataReader::file(void)
+{
+    return d->file;
+}
+
+const QStringList& dtkAbstractDataReader::files(void)
+{
+    return d->files;
 }

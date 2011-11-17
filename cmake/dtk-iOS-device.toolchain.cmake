5886668e16624b0882142ef6ce07d0be8a4b8e6f
Julien Wintz <julien.wintz@inria.fr>
2011-11-17 10:17:57 +0100

    Removed no more used Carbon or Cocoa gui related classes.

--- cmake/dtk-iOS-device.toolchain.cmake
+++ cmake/dtk-iOS-device.toolchain.cmake
@@ -4,9 +4,9 @@
 ## Copyright (C) 2008-2011 - Julien Wintz, Inria.
 ## Created: Wed Nov 16 22:09:38 2011 (+0100)
 ## Version: $Id$
-## Last-Updated: Thu Nov 17 00:11:08 2011 (+0100)
+## Last-Updated: Thu Nov 17 09:40:10 2011 (+0100)
 ##           By: Julien Wintz
-##     Update #: 12
+##     Update #: 17
 ######################################################################
 ## 
 ### Commentary: 
@@ -28,11 +28,15 @@ set(DTK_IOS_DEVROOT "/Developer/Platforms/iPhoneOS.platform/Developer")
 set(DTK_IOS_SDKROOT "/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${DTK_IOS_SDKVER}.sdk")
 set(DTK_IOS_CC  "${DTK_IOS_DEVROOT}/usr/bin/gcc")
 set(DTK_IOS_CXX "${DTK_IOS_DEVROOT}/usr/bin/g++")
+set(DTK_IOS_QT "/Users/jwintz/Development/ios/qt-device/")
 
 cmake_force_c_compiler  (${DTK_IOS_CC}  GNU)
 cmake_force_cxx_compiler(${DTK_IOS_CXX} GNU)
 
-set(CMAKE_FIND_ROOT_PATH "${DTK_IOS_SDKROOT}" "${DTK_IOS_DEVROOT}")
+set(CMAKE_FIND_ROOT_PATH "${DTK_IOS_SDKROOT}" "${DTK_IOS_DEVROOT}" "${DTK_IOS_QT}")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(OPENGL_gl_LIBRARY  "-F${DTK_IOS_SDKROOT}/System/Library/Frameworks -framework OpenGLES")
+set(OPENGL_glu_LIBRARY "-F${DTK_IOS_SDKROOT}/System/Library/Frameworks -framework OpenGLES")

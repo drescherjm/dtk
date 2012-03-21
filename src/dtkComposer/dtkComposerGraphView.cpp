bcd5fb4c13ee592df956156c3392c6adc5448484
Julien Wintz <julien.wintz@inria.fr>
2012-03-21 11:49:36 +0100

    creator main window equipped with splitters.

--- src/dtkComposer/dtkComposerGraphView.cpp
+++ src/dtkComposer/dtkComposerGraphView.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008-2011 - Julien Wintz, Inria.
  * Created: Thu Feb  9 14:43:33 2012 (+0100)
  * Version: $Id$
- * Last-Updated: ven. févr. 24 15:52:29 2012 (+0100)
- *           By: Nicolas Niclausse
- *     Update #: 16
+ * Last-Updated: Wed Mar 21 11:25:08 2012 (+0100)
+ *           By: Julien Wintz
+ *     Update #: 21
  */
 
 /* Commentary: 
@@ -26,6 +26,8 @@ public:
 
 dtkComposerGraphView::dtkComposerGraphView(QWidget *parent) : QGraphicsView(parent),d(new dtkComposerGraphViewPrivate)
 {
+    this->setAttribute(Qt::WA_MacShowFocusRect, false);
+    this->setFrameStyle(QFrame::NoFrame);
     this->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
     this->setWindowTitle("Graph");
 }

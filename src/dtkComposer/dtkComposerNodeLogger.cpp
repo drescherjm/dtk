3adea5415ddbe3ca7f5d2be17515287889e0f2c2
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2012-03-26 14:11:18 +0200

    remove useless debug message

--- src/dtkComposer/dtkComposerNodeLogger.cpp
+++ src/dtkComposer/dtkComposerNodeLogger.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008-2011 -Nicolas Niclausse , Inria.
  * Created: Mon Feb 27 12:38:46 2012 (+0100)
  * Version: $Id$
- * Last-Updated: lun. mars 26 13:49:39 2012 (+0200)
+ * Last-Updated: lun. mars 26 14:11:03 2012 (+0200)
  *           By: Nicolas Niclausse
- *     Update #: 43
+ *     Update #: 44
  */
 
 /* Commentary:
@@ -45,7 +45,6 @@ dtkComposerNodeLogger::~dtkComposerNodeLogger(void)
 
 void dtkComposerNodeLogger::run(void)
 {
-    dtkInfo() << "run logger";
     foreach(QVariant q,  d->receiver->allData())
         if (q.canConvert(QVariant::String))
             dtkInfo() << q.toString();

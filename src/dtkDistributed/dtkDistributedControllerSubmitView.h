3877a2881106c5f98df32e0d1a3b1ab42f7b9def
Julien Wintz <julien.wintz@inria.fr>
2012-04-03 17:15:09 +0200

    Towards distributed submit view.

---
+++ src/dtkDistributed/dtkDistributedControllerSubmitView.h
@@ -0,0 +1,41 @@
+/* dtkDistributedControllerSubmitView.h --- 
+ * 
+ * Author: Julien Wintz
+ * Copyright (C) 2008-2011 - Julien Wintz, Inria.
+ * Created: Tue Apr  3 16:52:03 2012 (+0200)
+ * Version: $Id$
+ * Last-Updated: Tue Apr  3 17:14:27 2012 (+0200)
+ *           By: Julien Wintz
+ *     Update #: 4
+ */
+
+/* Commentary: 
+ * 
+ */
+
+/* Change log:
+ * 
+ */
+
+#ifndef DTKDISTRIBUTEDCONTROLLERSUBMITVIEW_H
+#define DTKDISTRIBUTEDCONTROLLERSUBMITVIEW_H
+
+#include "dtkDistributedExport.h"
+
+#include <QtGui>
+
+class dtkDistributedControllerSubmitViewPrivate;
+
+class DTKDISTRIBUTED_EXPORT dtkDistributedControllerSubmitView : public QFrame
+{
+    Q_OBJECT
+
+public:
+     dtkDistributedControllerSubmitView(QWidget *parent = 0);
+    ~dtkDistributedControllerSubmitView(void);
+
+private:
+    dtkDistributedControllerSubmitViewPrivate *d;
+};
+
+#endif

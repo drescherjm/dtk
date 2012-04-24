4a6d58d3e3ed22b0bfe25a3346c18f8b081a5ca2
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2012-04-24 17:17:00 +0200

    add piechart class + use it in headerview

--- src/dtkDistributed/dtkDistributedControllerHeaderView.cpp
+++ src/dtkDistributed/dtkDistributedControllerHeaderView.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008-2011 - Julien Wintz, Inria.
  * Created: Wed Apr  4 12:23:14 2012 (+0200)
  * Version: $Id$
- * Last-Updated: Thu Apr 19 15:17:39 2012 (+0200)
- *           By: Julien Wintz
- *     Update #: 87
+ * Last-Updated: mar. avril 24 17:15:05 2012 (+0200)
+ *           By: Nicolas Niclausse
+ *     Update #: 178
  */
 
 /* Commentary: 
@@ -19,12 +19,16 @@
 
 #include "dtkDistributedController.h"
 #include "dtkDistributedControllerHeaderView.h"
+#include "dtkDistributedNode.h"
+
+#include <dtkGui/dtkPieChart.h>
 
 class dtkDistributedControllerHeaderViewPrivate
 {
 public:
     QLabel *server;
     QLabel *stats;
+    dtkPieChart *pie;
 
 public:
     QString cluster;
@@ -43,9 +47,12 @@ dtkDistributedControllerHeaderView::dtkDistributedControllerHeaderView(QWidget *
     d->stats = new QLabel(this);
     d->stats->setStyleSheet("font-size: 10px;");
 
+    d->pie = new dtkPieChart(this);
+
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(d->server);
     layout->addWidget(d->stats);
+    layout->addWidget(d->pie);
 }
 
 dtkDistributedControllerHeaderView::~dtkDistributedControllerHeaderView(void)
@@ -81,6 +88,7 @@ void dtkDistributedControllerHeaderView::onDisconnected(const QUrl& cluster)
 {
     d->server->clear();
     d->stats->clear();
+    d->pie->clear();
 
     d->cluster = QString();
 }
@@ -88,10 +96,36 @@ void dtkDistributedControllerHeaderView::onDisconnected(const QUrl& cluster)
 void dtkDistributedControllerHeaderView::update(void)
 {
     d->server->setText(QUrl(d->cluster).host());
-    
+    int nfree   =0;
+    int nbusy   =0;
+    int ndown   =0;
+    int nstandby=0;
+    int nabsent =0;
+    int total   =0;
+    foreach (dtkDistributedNode * node, d->controller->nodes(d->cluster)) {
+        total++;
+        if (node->state() == dtkDistributedNode::Free)
+            nfree++;
+        else if (node->state() == dtkDistributedNode::Busy)
+            nbusy++;
+        else if (node->state() == dtkDistributedNode::Down)
+            ndown++;
+        else if (node->state() == dtkDistributedNode::StandBy)
+            nstandby++;
+        else if (node->state() == dtkDistributedNode::Absent)
+            nabsent++;
+    }
     d->stats->setText(QString("Nodes: %1\nJobs: %2")
-                      .arg(d->controller->nodes(d->cluster).count())
-                      .arg(d->controller->jobs(d->cluster).count())); 
+                      .arg(total)
+                      .arg(d->controller->jobs(d->cluster).count()));
 
+    if (total > 0) {
+        d->pie->clear();
+        d->pie->addPiece("free: "   +QString::number(nfree),   100*nfree/total,   Qt::darkGreen);
+        d->pie->addPiece("dead: "   +QString::number(ndown),   100*ndown/total,   Qt::red);
+        d->pie->addPiece("standby: "+QString::number(nstandby),100*nstandby/total,Qt::blue);
+        d->pie->addPiece("busy: "   +QString::number(nbusy),   100*nbusy/total,   QColor("#FF7722"));
+        d->pie->addPiece("absent: " +QString::number(nabsent), 100*nabsent/total, Qt::black);
+    }
     QFrame::update();
 }

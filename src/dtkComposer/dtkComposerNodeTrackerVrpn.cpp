41195baf2b9bed4448c3eb439378ea4d243e9b7e
Thibaud Kloczko <Thibaud.Kloczko@inria.fr>
2012-09-25 10:32:44 +0200

    Update nodes for virtual reality.

--- src/dtkComposer/dtkComposerNodeTrackerVrpn.cpp
+++ src/dtkComposer/dtkComposerNodeTrackerVrpn.cpp
@@ -4,9 +4,9 @@
  * Copyright (C) 2008-2011 - Julien Wintz, Inria.
  * Created: Thu Apr 26 16:51:45 2012 (+0200)
  * Version: $Id$
- * Last-Updated: Thu Apr 26 17:58:33 2012 (+0200)
- *           By: Julien Wintz
- *     Update #: 12
+ * Last-Updated: Tue Sep 25 10:29:15 2012 (+0200)
+ *           By: tkloczko
+ *     Update #: 15
  */
 
 /* Commentary: 
@@ -51,9 +51,12 @@ dtkComposerNodeTrackerVrpn::dtkComposerNodeTrackerVrpn(void) : dtkComposerNodeLe
 
 dtkComposerNodeTrackerVrpn::~dtkComposerNodeTrackerVrpn(void)
 {
-    d->tracker->uninitialize();
-
-    delete d->tracker;
+    if (d->tracker) {
+        d->tracker->uninitialize();
+        delete d->tracker;
+    }
+    d->tracker = NULL;
+    
     delete d;
 
     d = NULL;
@@ -63,13 +66,13 @@ void dtkComposerNodeTrackerVrpn::run(void)
 {
     if(!d->tracker && !d->url.isEmpty()) {
         d->tracker = new dtkVrTrackerVrpn;
-        d->tracker->setUrl(QUrl(d->url.data()));
+        d->tracker->setUrl(QUrl(*d->url.data()));
         d->tracker->initialize();
     }
 
     if(!d->tracker)
         return;
 
-    d->head_position.setData(d->tracker->headPosition());
-    d->head_orientation.setData(d->tracker->headOrientation());
+    d->head_position.setData(*d->tracker->headPosition());
+    d->head_orientation.setData(*d->tracker->headOrientation());
 }

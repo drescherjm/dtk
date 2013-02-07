0a75b7b25aec01d8103683f281047bc716a24df8
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2013-02-07 15:20:43 +0100

    first version of dtkDistributedCommunicator API

---
+++ src/dtkDistributed/dtkDistributedCommunicator.cpp
@@ -0,0 +1,88 @@
+/* @(#)dtkDistributedCommunicator.cpp ---
+ *
+ * Author: Nicolas Niclausse
+ * Copyright (C) 2013 - Nicolas Niclausse, Inria.
+ * Created: 2013/02/07 13:59:41
+ */
+
+/* Commentary:
+ *
+ */
+
+/* Change log:
+ *
+ */
+
+#include "dtkDistributedCommunicator.h"
+
+// #include <dtk/dtkGlobal.h>
+
+dtkDistributedCommunicator::dtkDistributedCommunicator(void) : QObject()
+{
+
+}
+
+dtkDistributedCommunicator::~dtkDistributedCommunicator(void)
+{
+    // delete d;
+
+    // d = NULL;
+}
+
+dtkDistributedCommunicator::dtkDistributedCommunicator(const dtkDistributedCommunicator& other)
+{
+
+}
+
+dtkDistributedCommunicator& dtkDistributedCommunicator::operator = (const dtkDistributedCommunicator& other)
+{
+    return *this;
+
+}
+
+void dtkDistributedCommunicator::initialize(void)
+{
+   // DTK_DEFAULT_IMPLEMENTATION;
+}
+
+bool dtkDistributedCommunicator::initialized(void)
+{
+   // DTK_DEFAULT_IMPLEMENTATION;
+
+   return false;
+}
+
+void dtkDistributedCommunicator::uninitialize(void)
+{
+   // DTK_DEFAULT_IMPLEMENTATION;
+}
+
+int dtkDistributedCommunicator::rank(void)
+{
+   // DTK_DEFAULT_IMPLEMENTATION;
+   return -1;
+}
+
+int dtkDistributedCommunicator::size(void)
+{
+   // DTK_DEFAULT_IMPLEMENTATION;
+   return 0;
+}
+
+qlonglong dtkDistributedCommunicator::allocate(qlonglong count, qlonglong size, void *buffer)
+{
+    return -1;
+}
+
+QByteArray dtkDistributedCommunicator::get(qint32 from, qlonglong position ,  qlonglong size, qlonglong buffer_id)
+{
+    return QByteArray();
+}
+
+void dtkDistributedCommunicator::put(qint32 dest, qlonglong position, qlonglong size, const QByteArray &data, qlonglong buffer_id)
+{
+
+}
+
+// template<class T> T * get(qint32 from, qlonglong position, qlonglong size, qlonglong buffer_id);
+// template<class T> void put(qint32 dest, qlonglong position, qlonglong size, const T &data, qlonglong buffer_id);

c2c85af72052e9bbe14a93335ec702033c0f948a
Julien Wintz <julien.wintz@me.com>
2013-02-08 16:59:22 +0100

    Force ini format and user scope for per layer settings.

--- src/dtkCore/dtkCoreSettings.cpp
+++ src/dtkCore/dtkCoreSettings.cpp
@@ -3,9 +3,9 @@
  * Author: Julien Wintz
  * Created: Fri Feb  8 16:53:22 2013 (+0100)
  * Version: 
- * Last-Updated: Fri Feb  8 16:55:20 2013 (+0100)
+ * Last-Updated: Fri Feb  8 16:58:30 2013 (+0100)
  *           By: Julien Wintz
- *     Update #: 2
+ *     Update #: 3
  */
 
 /* Change Log:
@@ -14,7 +14,7 @@
 
 #include "dtkCoreSettings.h"
 
-dtkCoreSettings::dtkCoreSettings(void) : QSettings("inria", "dtk")
+dtkCoreSettings::dtkCoreSettings(void) : QSettings(QSettings::IniFormat, QSettings::UserScope, "inria", "dtk")
 {
 
 }

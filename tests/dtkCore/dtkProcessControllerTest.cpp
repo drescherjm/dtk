799dc87e74a38db0001c62ee68f9cd1c67bad3cf
Julien Wintz <julien.wintz@me.com>
2013-08-29 17:20:49 +0200

    Adding process controller to be able to kill any abstract process while running. Together with test. To be improved when used within an event loop, and optionally to retrieve data.

---
+++ tests/dtkCore/dtkProcessControllerTest.cpp
@@ -0,0 +1,118 @@
+/* dtkProcessControllerTest.cpp ---
+ * 
+ * Author: Julien Wintz
+ * Created: Thu Aug 29 14:50:23 2013 (+0200)
+ * Version: 
+ * Last-Updated: Thu Aug 29 17:17:16 2013 (+0200)
+ *           By: Julien Wintz
+ *     Update #: 97
+ */
+
+/* Change Log:
+ * 
+ */
+
+#include "dtkProcessControllerTest.h"
+
+#include <dtkCore/dtkAbstractProcess>
+#include <dtkCore/dtkAbstractProcessController>
+#include <dtkCore/dtkAbstractProcessFactory>
+
+// ///////////////////////////////////////////////////////////////////
+// 
+// ///////////////////////////////////////////////////////////////////
+
+class dummyProcess : public dtkAbstractProcess
+{
+public:
+    int update(void);
+
+public:
+    int count;
+};
+
+int dummyProcess::update(void)
+{
+    count = 0;
+
+    forever {
+	count++;
+	sleep(1);
+    }
+}
+
+// ///////////////////////////////////////////////////////////////////
+// dtkProcessControllerTestPrivate
+// ///////////////////////////////////////////////////////////////////
+
+class dtkProcessControllerTestObjectPrivate
+{
+public:
+    dtkAbstractProcessController *controller;
+};
+
+// ///////////////////////////////////////////////////////////////////
+// 
+// ///////////////////////////////////////////////////////////////////
+
+dtkProcessControllerTestObject::dtkProcessControllerTestObject(void)
+{
+    d = new dtkProcessControllerTestObjectPrivate;
+}
+
+dtkProcessControllerTestObject::~dtkProcessControllerTestObject(void)
+{
+    delete d;
+}
+
+void dtkProcessControllerTestObject::initTestCase(void)
+{
+    d->controller = new dtkAbstractProcessController;
+}
+
+void dtkProcessControllerTestObject::init(void)
+{
+
+}
+
+void dtkProcessControllerTestObject::testStart(void)
+{
+    d->controller->setProcess(new dummyProcess);
+    d->controller->start();
+}
+
+void dtkProcessControllerTestObject::testProcess(void)
+{
+    using namespace QTest;
+
+    qWait(2000); QCOMPARE(dynamic_cast<dummyProcess *>(d->controller->process())->count, 3);
+    qWait(2000); QCOMPARE(dynamic_cast<dummyProcess *>(d->controller->process())->count, 5);
+    qWait(2000); QCOMPARE(dynamic_cast<dummyProcess *>(d->controller->process())->count, 7);
+    qWait(2000); QCOMPARE(dynamic_cast<dummyProcess *>(d->controller->process())->count, 9);
+    qWait(2000); QCOMPARE(dynamic_cast<dummyProcess *>(d->controller->process())->count, 11);
+    qWait(2000); QCOMPARE(dynamic_cast<dummyProcess *>(d->controller->process())->count, 13);
+}
+
+void dtkProcessControllerTestObject::testStop(void)
+{
+    using namespace QTest;
+
+    qWait(5000);
+
+    QVERIFY(dynamic_cast<dummyProcess *>(d->controller->process())->count > 13);
+
+    d->controller->stop();
+}
+
+void dtkProcessControllerTestObject::cleanup(void)
+{
+
+}
+
+void dtkProcessControllerTestObject::cleanupTestCase(void)
+{
+    delete d->controller->process();
+    delete d->controller;
+}
+
+DTKTEST_NOGUI_MAIN(dtkProcessControllerTest, dtkProcessControllerTestObject)

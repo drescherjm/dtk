670087a45c275bf63ca28320e9379d623f6d29c9
Etienne Delclaux <etienne.delclaux@inria.fr>
2013-10-10 19:26:44 +0200

    View bugs fix. Close all Btn added

--- src/dtkGui/dtkViewListControl.h
+++ src/dtkGui/dtkViewListControl.h
@@ -1,20 +1,20 @@
-/* dtkViewListControl.h --- 
- * 
+/* dtkViewListControl.h ---
+ *
  * Author: Julien Wintz
  * Copyright (C) 2008-2011 - Julien Wintz, Inria.
  * Created: Wed Oct 31 12:49:39 2012 (+0100)
  * Version: $Id$
- * Last-Updated: Wed Oct 31 14:26:37 2012 (+0100)
- *           By: Julien Wintz
- *     Update #: 17
+ * Last-Updated: jeu. oct. 10 16:14:56 2013 (+0200)
+ *           By: Etienne
+ *     Update #: 18
  */
 
-/* Commentary: 
- * 
+/* Commentary:
+ *
  */
 
 /* Change log:
- * 
+ *
  */
 
 #ifndef DTKVIEWLISTCONTROL_H
@@ -44,6 +44,7 @@ protected slots:
     void onLayoutHorizontally(void);
     void onLayoutVertically(void);
     void onLayoutGrid(void);
+    void onLayoutCloseAll(void);
 
 private:
     dtkViewListControlPrivate *d;

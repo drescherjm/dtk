1041ef3014c5398a224d3da7dac2a41ffb8883dc
Thibaud Kloczko <thibaud.kloczko@inria.fr>
2014-05-12 17:05:30 +0200

    Add layer meta.

---
+++ src/dtkMeta/dtkMeta.h
@@ -0,0 +1,112 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+#include <QtCore>
+
+// /////////////////////////////////////////////////////////////////
+// dtkMetaTypeHandler definition
+// /////////////////////////////////////////////////////////////////
+
+template< typename T, bool = QtPrivate::IsPointerToTypeDerivedFromQObject<T>::Value > struct dtkMetaTypeHandler;
+
+template<typename T> struct dtkMetaTypeHandler<T *, false>
+{
+    static QVariant  variantFromValue(T *t);
+    static        T *clone(T *t);
+};
+
+template<typename T> struct dtkMetaTypeHandler<T *, true>
+{
+    static QVariant variantFromValue(T *t);
+    static T *clone(T *t);
+};
+
+// /////////////////////////////////////////////////////////////////
+// dtkMetaType definition
+// /////////////////////////////////////////////////////////////////
+
+class dtkMetaType
+{
+public:
+    template<typename T> static QVariant variantFromValue(const T& t);
+    template<typename T> static QVariant variantFromValue(      T *t);
+    template<typename T> static T *clone(T *t);
+};
+
+// /////////////////////////////////////////////////////////////////
+// Stream operators redefinition
+// /////////////////////////////////////////////////////////////////
+
+template <typename T> QDataStream& operator << (QDataStream& s, T *  t);
+template <typename T> QDataStream& operator >> (QDataStream& s, T *& t);
+
+template<typename T> QDataStream& operator << (QDataStream& s, const QList<T *>& l);
+template<typename T> QDataStream& operator >> (QDataStream& s,       QList<T *>& l);
+
+template<typename T> QDataStream& operator << (QDataStream& s, const QVector<T *>& v);
+template<typename T> QDataStream& operator >> (QDataStream& s,       QVector<T *>& v);
+
+template<typename T> QDataStream& operator << (QDataStream& s, const std::list<T *>& l);
+template<typename T> QDataStream& operator >> (QDataStream& s,       std::list<T *>& l);
+
+template<typename T> QDataStream& operator << (QDataStream& s, const std::vector<T *>& v);
+template<typename T> QDataStream& operator >> (QDataStream& s,       std::vector<T *>& v);
+
+// /////////////////////////////////////////////////////////////////
+// MACRO REGISTERING POINTERS OF CONTAINERS INTO METATYPE SYSTEM
+// /////////////////////////////////////////////////////////////////
+
+#define DTK_DECLARE_SEQUENTIAL_CONTAINER_POINTER(CONTAINER_ARG)                   \
+QT_BEGIN_NAMESPACE                                                                \
+template <typename T> struct QMetaTypeId< CONTAINER_ARG<T> *>                     \
+{                                                                                 \
+    enum {                                                                        \
+        Defined = QMetaTypeId2<T>::Defined                                        \
+    };                                                                            \
+    static int qt_metatype_id()                                                   \
+    {                                                                             \
+        static QBasicAtomicInt metatype_id = Q_BASIC_ATOMIC_INITIALIZER(0);       \
+        if (const int id = metatype_id.load())                                    \
+            return id;                                                            \
+        const char *tName = QMetaType::typeName(qMetaTypeId<T>());                \
+        Q_ASSERT(tName);                                                          \
+        const int tNameLen = int(qstrlen(tName));                                 \
+        QByteArray typeName;                                                      \
+        typeName.reserve(int(sizeof(#CONTAINER_ARG)) + 1 + tNameLen + 1 + 1 + 1); \
+        typeName.append(#CONTAINER_ARG, int(sizeof(#CONTAINER_ARG)) - 1)          \
+            .append('<').append(tName, tNameLen).append('*');                     \
+        typeName.append('>');                                                     \
+        const int newId = qRegisterNormalizedMetaType< CONTAINER_ARG<T> *>(       \
+                        typeName,                                                 \
+                        reinterpret_cast< CONTAINER_ARG<T> **>(quintptr(-1)));    \
+        metatype_id.storeRelease(newId);                                          \
+        return newId;                                                             \
+    }                                                                             \
+};                                                                                \
+namespace QtPrivate {                                                             \
+template<typename T> struct IsSequentialContainer<CONTAINER_ARG<T> *>             \
+{                                                                                 \
+    enum { Value = true };                                                        \
+};                                                                                \
+}                                                                                 \
+QT_END_NAMESPACE
+
+// /////////////////////////////////////////////////////////////////
+
+#include "dtkMeta.tpp"
+
+//
+// dtkMeta.h ends here
---
+++ src/dtkMeta/dtkMeta.tpp
@@ -0,0 +1,169 @@
+// Version: $Id$
+//
+//
+
+// Commentary:
+//
+//
+
+// Change Log:
+//
+//
+
+// Code:
+
+#pragma once
+
+#include <QtCore>
+
+// /////////////////////////////////////////////////////////////////
+// dtkMetaTypeHandler implementation
+// /////////////////////////////////////////////////////////////////
+
+template<typename T> inline QVariant dtkMetaTypeHandler<T *, false>::variantFromValue(T *t)
+{
+    return QVariant::fromValue(t);
+}
+
+template<typename T> inline T *dtkMetaTypeHandler<T *, false>::clone(T *t)
+{
+    return new T(*t);
+}
+
+template<typename T> inline QVariant dtkMetaTypeHandler<T *, true>::variantFromValue(T *t)
+{
+    return QVariant(QMetaType::type(qPrintable(QString(t->metaObject()->className()) + "*")), &t, 1);
+}
+
+template<typename T> inline T *dtkMetaTypeHandler<T *, true>::clone(T *t)
+{
+    return static_cast<T *>(QMetaType::create(QMetaType::type(qPrintable(QString(t->metaObject()->className()))), t));
+}
+
+// /////////////////////////////////////////////////////////////////
+// dtkMetaType implementation
+// /////////////////////////////////////////////////////////////////
+
+template<typename T> inline QVariant dtkMetaType::variantFromValue(const T& t)
+{
+    return QVariant::fromValue(t);
+}
+
+template<typename T> inline QVariant dtkMetaType::variantFromValue(T *t)
+{
+    return dtkMetaTypeHandler<T *>::variantFromValue(t);
+}
+
+template<typename T> inline T *dtkMetaType::clone(T *t)
+{
+    return dtkMetaTypeHandler<T *>::clone(t);
+}
+
+// /////////////////////////////////////////////////////////////////
+// Stream operators implementation
+// /////////////////////////////////////////////////////////////////
+
+
+template <typename T> inline QDataStream& operator << (QDataStream& s, T *t)
+{
+    s << *t;
+    return s;
+}
+
+template <typename T> inline QDataStream& operator >> (QDataStream& s, T *& t)
+{
+    if (!t)
+        t = new T();
+
+    s >> *t;
+    return s;
+}
+
+template<typename T> inline QDataStream& operator << (QDataStream& s, const QList<T *>& l)
+{
+    s << quint32(l.size());
+    for (int i = 0; i < l.size(); ++i)
+        s << dtkMetaType::variantFromValue(l.at(i));
+    return s;
+}
+
+template<typename T> inline QDataStream& operator >> (QDataStream& s, const QList<T *>& l)
+{
+    l.clear();
+    quint32 c; s >> c;
+    QVariant var;
+    for(quint32 i = 0; i < c; ++i) {
+        s >> var;
+        l << var.value<T *>();
+        if (s.atEnd())
+            break;
+    }
+    return s;
+}
+
+template<typename T> inline QDataStream& operator << (QDataStream& s, const QVector<T *>& v)
+{
+    s << quint32(v.size());
+    for (typename QVector<T *>::const_iterator it = v.begin(); it != v.end(); ++it)
+        s << dtkMetaType::variantFromValue(*it);
+    return s;
+}
+
+template<typename T> inline QDataStream& operator >> (QDataStream& s, QVector<T *>& v)
+{
+    v.clear();
+    quint32 c; s >> c;
+    v.resize(c);
+    QVariant var;
+    for(quint32 i = 0; i < c; ++i) {
+        s >> var;
+        v[i] = var.value<T *>();
+    }
+    return s;
+}
+
+template<typename T> inline QDataStream& operator << (QDataStream& s, const std::list<T *>& l)
+{
+    s << quint32(l.size());
+    for (typename std::list<T *>::const_iterator it = l.begin(); it != l.end(); ++it)
+        s << dtkMetaType::variantFromValue(*it);
+    return s;
+}
+
+template<typename T> inline QDataStream& operator >> (QDataStream& s, const std::list<T *>& l)
+{
+    l.clear();
+    quint32 c; s >> c;
+    QVariant var;
+    for(quint32 i = 0; i < c; ++i) {
+        s >> var;
+        l.push_back(var.value<T *>());
+        if (s.atEnd())
+            break;
+    }
+    return s;
+}
+
+template<typename T> inline QDataStream& operator << (QDataStream& s, const std::vector<T *>& v)
+{
+    s << quint32(v.size());
+    for (typename std::vector<T *>::const_iterator it = v.begin(); it != v.end(); ++it)
+        s << dtkMetaType::variantFromValue(*it);
+    return s;
+}
+
+template<typename T> inline QDataStream& operator >> (QDataStream& s, const std::vector<T *>& v)
+{
+    v.clear();
+    quint32 c; s >> c;
+    v.resize(c);
+    QVariant var;
+    for(quint32 i = 0; i < c; ++i) {
+        s >> var;
+        v[i] = var.value<T *>();
+    }
+    return s;
+}
+
+//
+// dtkMeta.tpp ends here

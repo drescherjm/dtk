c22e81b987324ddc7a34b82aea40d3905911cf51
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2014-10-23 13:41:38 +0200

    add missing file

---
+++ src/dtkComposer/dtkComposerMetaType.h
@@ -0,0 +1,29 @@
+/* dtkComposerMetaType.h --- 
+ * 
+ * Author: tkloczko
+ * Copyright (C) 2011 - Thibaud Kloczko, Inria.
+ * Created: Sat Aug  4 00:26:47 2012 (+0200)
+ */
+
+/* Commentary:
+ *
+ */
+
+/* Change log:
+ *
+ */
+
+#pragma once
+
+#include <QtCore>
+#include <QImage>
+
+// /////////////////////////////////////////////////////////////////
+//
+// /////////////////////////////////////////////////////////////////
+
+Q_DECLARE_METATYPE(QImage*);
+
+Q_DECLARE_METATYPE(QStringList*);
+Q_DECLARE_METATYPE(QByteArray *);
+

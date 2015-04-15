cac8c1c1e74a3ed82629c0a5aaba6978cb9434a5
Thibaud Kloczko <thibaud.kloczko@inria.fr>
2015-04-15 13:42:28 +0200

    Add operation handler to buffer manager.

--- src/dtkDistributed/dtkDistributedBufferManager.h
+++ src/dtkDistributed/dtkDistributedBufferManager.h
@@ -14,7 +14,68 @@
 
 #pragma once
 
+#include <dtkCore>
+
 #include "dtkDistributedExport.h"
+
+// ///////////////////////////////////////////////////////////////////
+// dtkDistributedBufferOperationManager
+// ///////////////////////////////////////////////////////////////////
+
+struct DTKDISTRIBUTED_EXPORT dtkDistributedBufferOperationManager
+{
+public:
+    virtual ~dtkDistributedBufferOperationManager(void) {;}
+
+public:
+    virtual void addAssign(char *result, void *source, qlonglong count) = 0;
+    virtual void subAssign(char *result, void *source, qlonglong count) = 0;
+    virtual void mulAssign(char *result, void *source, qlonglong count) = 0;
+    virtual void divAssign(char *result, void *source, qlonglong count) = 0;
+};
+
+// ///////////////////////////////////////////////////////////////////
+// dtkDistributedBufferOperationManagerTyped
+// ///////////////////////////////////////////////////////////////////
+
+template <typename T> struct dtkDistributedBufferOperationManagerTyped : public dtkDistributedBufferOperationManager
+{
+public:
+    void addAssign(char *result, void *source, qlonglong count);
+    void subAssign(char *result, void *source, qlonglong count);
+    void mulAssign(char *result, void *source, qlonglong count);
+    void divAssign(char *result, void *source, qlonglong count);
+};
+
+// ///////////////////////////////////////////////////////////////////
+
+template <typename T> inline void dtkDistributedBufferOperationManagerTyped<T>::addAssign(char *result, void *source, qlonglong count)
+{
+    dtkArray<T> r = dtkArray<T>::fromWritableRawData(reinterpret_cast<T *>(result), count);
+    const dtkArray<T> s = dtkArray<T>::fromRawData(reinterpret_cast<T *>(source), count);
+    std::transform(r.begin(), r.end(), s.begin(), r.begin(), std::plus<T>());
+}
+
+template <typename T> inline void dtkDistributedBufferOperationManagerTyped<T>::subAssign(char *result, void *source, qlonglong count)
+{
+    dtkArray<T> r = dtkArray<T>::fromWritableRawData(reinterpret_cast<T *>(result), count);
+    const dtkArray<T> s = dtkArray<T>::fromRawData(reinterpret_cast<T *>(source), count);
+    std::transform(r.begin(), r.end(), s.begin(), r.begin(), std::minus<T>());
+}
+
+template <typename T> inline void dtkDistributedBufferOperationManagerTyped<T>::mulAssign(char *result, void *source, qlonglong count)
+{
+    dtkArray<T> r = dtkArray<T>::fromWritableRawData(reinterpret_cast<T *>(result), count);
+    const dtkArray<T> s = dtkArray<T>::fromRawData(reinterpret_cast<T *>(source), count);
+    std::transform(r.begin(), r.end(), s.begin(), r.begin(), std::multiplies<T>());
+}
+
+template <typename T> inline void dtkDistributedBufferOperationManagerTyped<T>::divAssign(char *result, void *source, qlonglong count)
+{
+    dtkArray<T> r = dtkArray<T>::fromWritableRawData(reinterpret_cast<T *>(result), count);
+    const dtkArray<T> s = dtkArray<T>::fromRawData(reinterpret_cast<T *>(source), count);
+    std::transform(r.begin(), r.end(), s.begin(), r.begin(), std::divides<T>());
+}
 
 // ///////////////////////////////////////////////////////////////////
 // dtkDistributedBufferManager
@@ -23,14 +84,15 @@
 class DTKDISTRIBUTED_EXPORT dtkDistributedBufferManager
 {
 public:
-    virtual ~dtkDistributedBufferManager(void) {;}
+             dtkDistributedBufferManager(void);
+    virtual ~dtkDistributedBufferManager(void);
 
 public:
     template <typename T>    T *allocate(qlonglong capacity);
     template <typename T> void  deallocate(T *& buffer);
     
 protected:
-    virtual void *allocate(qlonglong objectSize, qlonglong capacity) = 0;
+    virtual void *allocate(qlonglong objectSize, qlonglong capacity, int metatype_id) = 0;
     virtual void  deallocate(void *buffer, qlonglong objectSize) = 0;
 
 public:
@@ -48,20 +110,47 @@ public:
     virtual void get(qint32 from, qlonglong position, void *array, qlonglong count = 1) = 0;
     virtual void put(qint32 dest, qlonglong position, void *array, qlonglong count = 1) = 0;
     virtual void addAssign(qint32 dest, qlonglong position, void *array, qlonglong nelements = 1) = 0;
+    virtual void mulAssign(qint32 dest, qlonglong position, void *array, qlonglong nelements = 1) = 0;
 
+protected:
+    virtual bool canHandleOperationManager(void);
+
+protected:
+    dtkDistributedBufferOperationManager *operation_manager;
 };
 
 // ///////////////////////////////////////////////////////////////////
 // dtkDistributedBufferManager templated member functions
 // ///////////////////////////////////////////////////////////////////
 
+inline dtkDistributedBufferManager::dtkDistributedBufferManager(void) : operation_manager(NULL) 
+{
+}
+
+inline dtkDistributedBufferManager::~dtkDistributedBufferManager(void)
+{
+}
+
+inline bool dtkDistributedBufferManager::canHandleOperationManager(void)
+{
+    return true;
+}
+
 template <typename T> inline T *dtkDistributedBufferManager::allocate(qlonglong capacity)
 {
-    return static_cast<T *>(this->allocate(sizeof(T), capacity));
+    if (this->canHandleOperationManager() && !this->operation_manager)
+        this->operation_manager = new dtkDistributedBufferOperationManagerTyped<T>();
+
+    return static_cast<T *>(this->allocate(sizeof(T), capacity, qMetaTypeId<T>()));
 }
 
 template <typename T> inline void dtkDistributedBufferManager::deallocate(T *& buffer)
 {
+    if (this->operation_manager && this->canHandleOperationManager()) {
+        delete this->operation_manager;
+        this->operation_manager = NULL;
+    }
+
     this->deallocate(buffer, sizeof(T));
     buffer = NULL;
 }

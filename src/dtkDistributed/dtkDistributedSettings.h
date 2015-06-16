79e1e43adad6031e67b0983b6d007616508e9e0c
Nicolas Niclausse <Nicolas.Niclausse@inria.fr>
2015-06-16 13:40:21 +0200

    enhance API of dtkDistributedSettings; fixes for message parsing and controller

--- src/dtkDistributed/dtkDistributedSettings.h
+++ src/dtkDistributed/dtkDistributedSettings.h
@@ -23,6 +23,11 @@ class DTKDISTRIBUTED_EXPORT dtkDistributedSettings : public QSettings
 public:
      dtkDistributedSettings(void);
     ~dtkDistributedSettings(void);
+
+public:
+     QString        path(const QUrl server);
+     QString server_type(const QUrl server);
+     bool use_ssh_tunnel(const QUrl server);
 };
 
 //
